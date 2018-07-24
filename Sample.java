97765cfb5106f33c09f6da89b355df4aec288d0b
JKDemo <suryah.dj@gmail.com>
2018-07-24 22:58:27 +0530

    class file added

---
+++ Sample.java
@@ -0,0 +1,20 @@
+package org.test.sample;
+
+public class Sample {
+
+	public static void testCse1() {
+
+		System.out.println("test");
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+		testCse1();
+		
+	}
+
+}
