0c3bce0ab7b7db570f65ea74da2170d1c01b161e
JKDemo <suryah.dj@gmail.com>
2018-07-24 23:52:25 +0530

    new files added

---
+++ Sample2.java
@@ -0,0 +1,16 @@
+package org.test.sample;
+
+public class Sample2 {
+
+	
+	public static void testCse1() {
+
+		System.out.println("test");
+		
+	}
+	public static void main(String[] args) {
+
+		testCse1();
+	}
+
+}
